c0b095ac345dc43f7dcf300f808719b414968073
Sanzhar Yerzhanov <sanzar30062000@gmail.com>
2025-03-16 23:01:51 +0500

    add direct reply-to support

---
+++ Direct reply-to/Client/Publisher.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Concurrent;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class Publisher
+{
+    private static readonly ConcurrentDictionary<string, TaskCompletionSource<byte[]>> CallbackMapper = new();
+
+    public static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        await using var connection = await factory.CreateConnectionAsync();
+        await using var channel = await connection.CreateChannelAsync();
+        var replyToQueueName = "amq.rabbitmq.reply-to";
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += async (model, ea) =>
+        {
+            if (CallbackMapper.TryRemove(ea.BasicProperties.CorrelationId, out var tcs))
+            {
+                tcs.SetResult(ea.Body.ToArray());
+            }
+            await Task.Yield();
+        };
+
+        await channel.BasicConsumeAsync(
+            queue: replyToQueueName,
+            autoAck: true,
+            consumer: consumer
+        );
+
+        var correlationId = Guid.NewGuid().ToString();
+        var props = new BasicProperties
+        {
+            CorrelationId = correlationId,
+            ReplyTo = replyToQueueName
+        };
+        var message = "Hello, this is blendereru";
+        var body = Encoding.UTF8.GetBytes(message);
+        var tcs = new TaskCompletionSource<byte[]>();
+        CallbackMapper[correlationId] = tcs;
+        await channel.BasicPublishAsync(exchange: string.Empty, routingKey:"rpc_queue", true, basicProperties: props, body: body);
+        var responseBytes = await tcs.Task;
+        var responseMessage = Encoding.UTF8.GetString(responseBytes);
+        Console.WriteLine($"Received response: {responseMessage}");
+        Console.ReadLine();
+    }
+}
---
+++ Direct reply-to/Server/Consumer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class Consumer
+{
+    public static async Task Main()
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        await using var connection = await factory.CreateConnectionAsync();
+        await using var channel = await connection.CreateChannelAsync();
+        var queueName = "rpc_queue";
+        await channel.QueueDeclareAsync(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+        await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += async (model, ea) =>
+        {
+            var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+            Console.WriteLine($"[x] Received message: {message}");
+
+            var responseMessage = $"Message consumed: {message}";
+            var responseBytes = Encoding.UTF8.GetBytes(responseMessage);
+
+            var props = ea.BasicProperties;
+            var replyProps = new BasicProperties
+            {
+                CorrelationId = props.CorrelationId
+            };
+
+            await channel.BasicPublishAsync(
+                exchange: string.Empty,
+                routingKey: props.ReplyTo,
+                mandatory: true,
+                basicProperties: replyProps,
+                body: responseBytes
+            );
+            await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+            Console.WriteLine($"[x] Acknowledged message with delivery tag: {ea.DeliveryTag}");
+        };
+
+        await channel.BasicConsumeAsync(queue: queueName, autoAck: false, consumer: consumer);
+        Console.WriteLine($"Waiting for messages in queue '{queueName}'. Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
