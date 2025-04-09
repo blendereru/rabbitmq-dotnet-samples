dd81eaf7a9ee9969cab07c9a5cfb52730a0b4650
Sanzhar Yerzhanov <sanzar30062000@gmail.com>
2025-04-09 19:50:44 +0500

    add super stream sac api

---
+++ Streams/Super Streams/Single Active Consumer/SuperStreams.SAC.Consumer1/Consume.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using RabbitMQ.Stream.Client;
+using RabbitMQ.Stream.Client.Reliable;
+
+var streamSystem = await StreamSystem.Create(new StreamSystemConfig()).ConfigureAwait(false);
+var consumer = await Consumer.Create(new ConsumerConfig(streamSystem, "invoices")
+{
+    IsSuperStream = true,
+    Reference = "my-superstream-sac-consumer",
+    IsSingleActiveConsumer = true,
+    OffsetSpec = new OffsetTypeFirst(),
+    MessageHandler = async (stream, consumerSource, context, message) =>
+    {
+        var body = Encoding.UTF8.GetString(message.Data.Contents);
+        Console.WriteLine($"Received message id: {message.Properties.MessageId}, body: {body}, stream: {stream}, offset: {context.Offset}");
+        await consumerSource.StoreOffset(context.Offset).ConfigureAwait(false);
+        await Task.CompletedTask.ConfigureAwait(false);
+    },
+    ConsumerUpdateListener = async (consumerRef, stream, isActive) =>
+    {
+        ulong offset = 0;
+        try
+        {
+            offset = await streamSystem.QueryOffset(consumerRef, stream).ConfigureAwait(false);
+        }
+        catch (OffsetNotFoundException)
+        {
+            Console.WriteLine($"Offset not found for stream {stream} and consumer {consumerRef}. Will use the first offset");
+            return new OffsetTypeFirst();
+        }
+        await Task.CompletedTask.ConfigureAwait(false);
+        return new OffsetTypeOffset(offset + 1);
+    }
+}).ConfigureAwait(false);
+Console.WriteLine("Consumer is running. Press [enter] to exit.");
+Console.ReadLine();
+await consumer.Close().ConfigureAwait(false);
+await consumer.Close().ConfigureAwait(false);
---
+++ Streams/Super Streams/Single Active Consumer/SuperStreams.SAC.Publisher/Publish.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using RabbitMQ.Stream.Client;
+using RabbitMQ.Stream.Client.AMQP;
+using RabbitMQ.Stream.Client.Reliable;
+
+var streamSystem = await StreamSystem.Create(new StreamSystemConfig()).ConfigureAwait(false);
+var producer = await Producer.Create(new ProducerConfig(streamSystem, "invoices")
+{
+    SuperStreamConfig = new SuperStreamConfig()
+    {
+        Routing = msg => msg.Properties.MessageId.ToString(),
+    }
+}).ConfigureAwait(false);
+for (int i = 0; i < 100; i++)
+{
+    var message = new Message(Encoding.UTF8.GetBytes($"Hello, this is message #{i}"))
+    {
+        Properties = new Properties() { MessageId = $"id_{i}" },
+        ApplicationProperties = new ApplicationProperties()
+        {
+            {"Id", i}
+        }
+    };
+    await producer.Send(message).ConfigureAwait(false);
+}
+Console.WriteLine("Sending 100 messages to invoices");
+await producer.Close().ConfigureAwait(false);
+await streamSystem.Close().ConfigureAwait(false);
