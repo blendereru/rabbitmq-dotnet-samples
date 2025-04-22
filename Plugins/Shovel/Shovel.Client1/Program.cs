116c684d51b95e58984eb55a2e4fdc9c5a4d8216
Sanzhar Yerzhanov <sanzar30062000@gmail.com>
2025-04-22 17:31:41 +0500

    shovel(not working yet)

---
+++ Plugins/Shovel/Shovel.Client1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var connectionFactory = new ConnectionFactory() {HostName = "localhost", Port = 5672};
+await using var connection = await connectionFactory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+for (int i = 0; i < 100; i++)
+{
+    var body = Encoding.UTF8.GetBytes($"Message #{i}");
+    await channel.BasicPublishAsync(string.Empty, "shovel.src.queue", body);
+}
+Console.WriteLine("Published 100 messages to src.queue");
---
+++ Plugins/Shovel/Shovel.Client2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var connectionFactory = new ConnectionFactory() {HostName = "localhost", Port = 5674};
+await using var connection = await connectionFactory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var currentChannel = ((AsyncEventingBasicConsumer)model).Channel;
+    var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+    Console.WriteLine($"Received {message}");
+    await currentChannel.BasicAckAsync(ea.DeliveryTag, multiple: true);
+};
+await channel.BasicConsumeAsync("shovel.dest.queue", autoAck: false, consumer);
+Console.WriteLine("Waiting for messages in shovel.dest.queue. Press [enter] to exit");
+Console.ReadLine();
